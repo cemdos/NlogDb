dbd7afc1ebf3da0360b88e1d166bdfab591be350
Cem DOŞ <cem.dos@hotmail.com>
2020-04-23 19:52:50 +0300

    NLog

---
+++ LogData/Program.cs
@@ -0,0 +1,27 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogData
+{
+    class Program
+    {
+        Logger logger = LogManager.GetCurrentClassLogger();
+        static void Main(string[] args)
+        {
+            Logger logger = LogManager.GetCurrentClassLogger();
+            try
+            {
+                int zero = 0;
+                int result = 5 / zero;
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex,ex.Message);
+            }
+        }
+    }
+}
